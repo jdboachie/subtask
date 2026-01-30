7471200142f244e4904e5bc47898584fc4c44320
Jude Boachie <jude.boachie@amalitech.com>
2026-01-30 15:38:44 +0000

    Refactor app to use global app state

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,43 +1,32 @@
-import { Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Button } from './ui/button/button';
-import { Board, Sidebar } from './ui/sidebar/sidebar';
+import { AppState } from './app-state';
+import { HeaderBar } from './ui/header-bar/header-bar';
+import { Sidebar } from './ui/sidebar/sidebar';
 import { ShowSidebarButton } from './ui/sidebar/show-sidebar-button';
-import { Theme } from './ui/theme';
-import { ThemeToggle } from './ui/theme-toggle/theme-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ThemeToggle, Button, Sidebar, ShowSidebarButton],
+  imports: [RouterOutlet, Sidebar, ShowSidebarButton, HeaderBar],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  private readonly theme = inject(Theme);
-  protected readonly title = signal('subtask');
+  protected readonly appState = inject(AppState);
 
   protected readonly sidebarHidden = signal(false);
-  protected readonly activeBoardId = signal<string | null>('1');
 
-  protected readonly boards = signal<Board[]>([
-    { id: '1', name: 'Platform Launch' },
-    { id: '2', name: 'Marketing Plan' },
-    { id: '3', name: 'Roadmap' },
-  ]);
-
-  protected hideSidebar(): void {
-    this.sidebarHidden.set(true);
-  }
-
-  protected showSidebar(): void {
-    this.sidebarHidden.set(false);
-  }
-
-  protected selectBoard(id: string): void {
-    this.activeBoardId.set(id);
-  }
+  protected readonly addTaskDisabled = computed(() => {
+    const board = this.appState.currentBoard();
+    return !board || board.columns.length === 0;
+  });
 
   protected onCreateBoard(): void {
     console.log('Create new board');
+  }
+
+  protected onAddTask(): void {
+    console.log('Add new task');
   }
 }

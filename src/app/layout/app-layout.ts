917ae11f048ad536a824a26b9b78d079021cf705
Jude Boachie <jude.boachie@amalitech.com>
2026-02-18 17:01:20 +0000

    feat: persist sidebar hidden state with LocalSync

--- src/app/layout/app-layout.ts
+++ src/app/layout/app-layout.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { AppState } from '../app-state';
+import { LocalSync } from '../services/local-sync';
 import { HeaderBar } from '../ui/header-bar/header-bar';
 import { Sidebar } from '../ui/sidebar/sidebar';
 import { ShowSidebarButton } from '../ui/sidebar/show-sidebar-button';
@@ -15,8 +16,14 @@ import { ShowSidebarButton } from '../ui/sidebar/show-sidebar-button';
 export class AppLayout {
   protected appState = inject(AppState);
   private readonly router = inject(Router);
+  private readonly localSync = inject(LocalSync);
 
   protected readonly sidebarHidden = signal(false);
+
+  constructor() {
+    this.localSync.init('subtask.sidebarHidden', this.sidebarHidden);
+    this.localSync.sync('subtask.sidebarHidden', this.sidebarHidden);
+  }
 
   protected readonly addTaskDisabled = computed(() => {
     const board = this.appState.currentBoard();
